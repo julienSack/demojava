ffdbcab9aba83fbf62ac0d82df8cf577da64643a
julien.sack <julien.sack@ipilyon.net>
2018-12-17 16:46:24 +0100

    avant le JS

--- src/main/java/com/ipiecoles/javaexo/mesi/MesiApplication.java
+++ src/main/java/com/ipiecoles/javaexo/mesi/MesiApplication.java
@@ -1,13 +1,18 @@
 package com.ipiecoles.javaexo.mesi;
 
-import org.springframework.boot.SpringApplication;
+import com.ipiecoles.javaexo.mesi.model.contact;
+import com.ipiecoles.javaexo.mesi.repository.ContactRepository;
+import 	org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MesiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MesiApplication.class, args);
+
+
 	}
 
 }
--- src/test/java/com/ipiecoles/javaexo/mesi/MesiApplicationTests.java
+++ src/test/java/com/ipiecoles/javaexo/mesi/MesiApplicationTests.java
@@ -1,7 +1,13 @@
 package com.ipiecoles.javaexo.mesi;
 
+import com.ipiecoles.javaexo.mesi.model.contact;
+import com.ipiecoles.javaexo.mesi.model.enfants;
+import com.ipiecoles.javaexo.mesi.repository.ContactRepository;
+import com.ipiecoles.javaexo.mesi.repository.EnfantsRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,9 +15,48 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MesiApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private EnfantsRepository enfantsRepository;
+    @Autowired
+    private ContactRepository contactRepository;
 
+    @Test
+    public void testConnexionBDD() {
+
+    }
+
+  /*  @Test
+    public void test() {
+        //Iterable<contact> contacts = contactRepository.findAll();
+        Iterable<enfants> listEnfants = enfantsRepository.findAll();
+        //for (enfants e : enfant) {
+            // System.out.println(e);
+        //}
+        {
+
+        }
+        //for (contact c : contacts) {
+            //     System.out.println(c);
+
+            for (enfants e : listEnfants) {
+                System.out.println(e);
+
+            }
+
+    }*/
+
+    @Test
+    public void testCheckBadIdContact(){
+        //Given = Initialisation des données d'entrée
+        Integer id_contact = 3;
+        contact a = new contact();
+
+        //When = Exécution de la méthode à tester
+
+        boolean b = contactRepository.exists(id_contact);
+
+        //Then = Vérifications de ce qu'a fait la méthode
+        Assertions.assertThat(b).isTrue();
+    }
 }
 
